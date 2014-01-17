2156327822e91cc54802abf84df54ebdeb2106a5
Filip Jukic <filip@jukic.me>
2014-01-17 23:22:17 +0100

    added ansible deploy scripts

---
+++ .docker/ansible/roles/miroapp/files/production_settings.py
@@ -0,0 +1,22 @@
+import os
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'advideotise',
+            'USER': 'admin',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_DB_PORT_3306_TCP_ADDR'),
+        'PORT': os.environ.get('MYSQL_DB_PORT_3306_TCP_PORT'),
+            'TEST_CHARSET': 'utf8',
+            'TEST_COLLATION': 'utf8_general_ci',
+        }
+    }
+
+HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+            'URL': "http://{0}:{1}".format(os.environ.get('ELASTIC_SEARCH_PORT_9200_TCP_ADDR'), os.environ.get('ELASTIC_SEARCH_PORT_9200_TCP_PORT')),
+            'INDEX_NAME': 'mirocommunity'
+            }
+        }
